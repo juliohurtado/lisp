1ffbeaaaef656ca5f7b036fb9829209ae932a2fc
Trevor John, trevorrjohn <pair+trevor@gmail.com>
2012-09-23 19:54:45 -0400

    First commit;

---
+++ guess-my-number.lisp
@@ -0,0 +1,22 @@
+(defparameter *big* 100)
+(defparameter *small* 1)
+
+(defun guess-my-number () 
+  (ash (+ *big* *small*) -1)
+)
+
+(defun smaller () 
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number)
+)
+
+(defun bigger () 
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number)
+)
+
+(defun start-over ()
+  (setf *small* 1)
+  (setf *big* 100)
+  (guess-my-number)
+)
